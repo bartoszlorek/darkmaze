09d61844528e7262239123acc3d7085942739092
Bartosz Lorek <bartosz.lorek@brainly.com>
2024-01-15 09:57:10 +0100

    remove TilesOutline

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,7 +4,6 @@ export * from "./dfs";
 export * from "./draw";
 export * from "./fieldOfView";
 export * from "./math";
-export * from "./outline";
 export * from "./pooling";
 export * from "./randomNumber";
 export * from "./randomSeed";
