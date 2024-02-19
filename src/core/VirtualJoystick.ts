b58186775bbfde7f0f5c22fedfc78a145954b940
Bartosz Lorek <bartosz.lorek@brainly.com>
2024-02-19 09:17:28 +0100

    virtual joystick separate axis interaction

--- src/core/VirtualJoystick.ts
+++ src/core/VirtualJoystick.ts
@@ -12,39 +12,41 @@ export class VirtualJoystick {
   }
 
   bind() {
+    let startClientX = 0;
+    let startClientY = 0;
     let lastClientX = 0;
     let lastClientY = 0;
 
     const handleStart = (e: TouchEvent) => {
-      lastClientX = e.touches[0].clientX;
-      lastClientY = e.touches[0].clientY;
+      startClientX = lastClientX = e.touches[0].clientX;
+      startClientY = lastClientY = e.touches[0].clientY;
       this.onStart();
     };
 
     const handleMove = (e: TouchEvent) => {
       const clientX = e.touches[0].clientX;
       const clientY = e.touches[0].clientY;
-      const diffX = Math.abs(lastClientX - clientX);
-      const diffY = Math.abs(lastClientY - clientY);
 
-      if (diffX >= this.thresholdX) {
-        if (lastClientX > clientX) {
-          this.onChangeLeft();
-        } else {
-          this.onChangeRight();
+      if (Math.abs(startClientX - clientX) > Math.abs(startClientY - clientY)) {
+        if (Math.abs(lastClientX - clientX) >= this.thresholdX) {
+          if (lastClientX > clientX) {
+            this.onChangeLeft();
+          } else {
+            this.onChangeRight();
+          }
+
+          lastClientX = clientX;
         }
+      } else {
+        if (Math.abs(lastClientY - clientY) >= this.thresholdY) {
+          if (lastClientY > clientY) {
+            this.onChangeUp();
+          } else {
+            this.onChangeDown();
+          }
 
-        lastClientX = clientX;
-      }
-
-      if (diffY >= this.thresholdY) {
-        if (lastClientY > clientY) {
-          this.onChangeUp();
-        } else {
-          this.onChangeDown();
+          lastClientY = clientY;
         }
-
-        lastClientY = clientY;
       }
     };
 
